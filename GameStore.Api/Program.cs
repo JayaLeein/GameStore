2be909ee4fd5ab51302b4652b35d581d7bfdaff1
JingYi Li <li000848@algonquinlive.com>
2024-04-01 22:12:43 -0400

    added a function of creating a new game

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -3,6 +3,7 @@
 // Host: to represent an HTTP server implementation for your app so that you can start listening for HTTP request
 // It also stands up a bunch of components and services
 
+using System.Reflection.Metadata.Ecma335;
 using GameStore.Api.Dtos;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,6 +11,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Here can always add Builder. in these two lines to configure a series of services
 
 var app = builder.Build();
+
+const string GetGameEndpointName = "GetGame";
 
 List<GameDto> games = [
     new (
@@ -43,7 +46,25 @@ app.MapGet("games", () => games);
 // app.MapGet("/", () => "Hello World!");
 
 // GET /games/1
-app.MapGet("games/{id}", (int id) => games.Find(game => game.Id == id));
+app.MapGet("games/{id}", (int id) => games.Find(game => game.Id == id))
+    .WithName(GetGameEndpointName);
+
+
+// POST /games
+// When somebody post something into the game's endpoint 
+app.MapPost("games", (CreateGameDto newGame) =>
+{
+    GameDto game = new(
+        games.Count + 1, // our id
+        newGame.Name,
+        newGame.Genre,
+        newGame.Price,
+        newGame.ReleaseDate);
+
+    games.Add(game);
+
+    return Results.CreatedAtRoute(GetGameEndpointName, new { id = game.Id}, game);
+});
 
 
 app.Run();
